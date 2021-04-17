1b56cecd020dfb482400f0a870740346628252ab
johmstone <johmstone@gmail.com>
2021-04-17 01:01:02 +0000

    update routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,12 +2,21 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, CryptoUser, User
+from api.models import db, CryptoUser
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
 
+@api.route('/hello', methods=['POST', 'GET'])
+def handle_hello():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+    
 @api.route('/user', methods=["GET"])
 def get_user ():
     users = CryptoUser.query.all()
