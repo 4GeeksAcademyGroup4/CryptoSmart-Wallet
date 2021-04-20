4a06f8d05e393afed40e921c189799ee2e22a2f3
hirolabpro <hirocastro@gmail.com>
2021-04-20 20:39:07 +0000

    jairo

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, CryptoUser
 from api.utils import generate_sitemap, APIException
+from flask_jwt import JWT, jwt_required, current_identity
 
 api = Blueprint('api', __name__)
 
