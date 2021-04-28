fc213b91c7e202d4a70139897c2adbbfd39f61d6
hirolabpro <hirocastro@gmail.com>
2021-04-28 03:01:03 +0000

    Styling

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -10,10 +10,13 @@ export const Register = () => {
 	const [inputSname, setSnameValue] = useState("");
 	const [inputEmail, setEmailValue] = useState("");
 	const [inputPassword, setPasswordValue] = useState("");
+	const [inputRpassword] = useState("");
 	const [user, setUser] = useState(false);
 
 	const handleSubmit = event => {
 		let baseURL = process.env.BACKEND_URL + "/api/Register";
+
+		let validURL = process.env.BACKEND_URL + "/api/ValidateEmail";
 
 		var myHeaders = new Headers();
 		myHeaders.append("Content-Type", "application/json");
@@ -48,7 +51,6 @@ export const Register = () => {
 				}
 			})
 			.then(result => {
-				console.log(result);
 				setUser(true);
 			})
 			.catch(error => console.log("error:", error));
@@ -58,104 +60,112 @@ export const Register = () => {
 
 	if (user) {
 		return (
-			<section className="container mw-90">
-				<div className="card-contact card border-0 mw-100 mx-auto bg-transparent">
-					<div className="text-center mt-3">
-						<h2 className="mb-0 mt-3 text-font-base">Su registro fue exitoso</h2>
-					</div>
+			<div id="cardRegister" className="animated fadeIn">
+				<div id="upper-side">
+					<i className="fa fa-check" />
+					<h3 id="status"> Registro Existoso </h3>
 				</div>
-			</section>
+				<div id="lower-side">
+					<p id="message">Su cuenta ha sido creada exitosamente</p>
+					<Link to="/Login" id="contBtn">
+						Continuar
+					</Link>
+				</div>
+			</div>
 		);
 	} else {
 		return (
-			<section className="container mw-90">
-				<div className="card-contact card border-0 mw-100 mx-auto bg-transparent">
+			<section className="container mw-90 bg-warning w-50">
+				<div id="maindiv" className="card-contact card border-5 border-white mw-100 mx-auto">
 					<div className="text-center mt-3">
-						<h2 className="mb-0 mt-3 text-font-base">Crea un nuevo usuario</h2>
+						<h2 className="mb-0 mt-3 text-font-base text-white">Unete a CryptoSmart Wallet</h2>
 						<br />
 					</div>
-					<article className="card-body mx-auto py-0">
-						<form onSubmit={handleSubmit}>
-							<div className="form-group input-group mb-0">
-								<div className="input-group-prepend">
-									<span className="input-group-text">
-										<i className="fas fa-user" />
-									</span>
-								</div>
-								<input
-									type="text"
-									value={inputFname}
-									placeholder="Nombre"
-									onChange={e => setFnameValue(e.target.value)}
-									className="form-control"
-									required
-								/>
-							</div>
 
-							<div className="form-group input-group mt-3 mb-0">
-								<div className="input-group-prepend">
-									<span className="input-group-text">
-										<i className="fas fa-user" />
-									</span>
+					<div id="articlediv" className="card-contact card border-0 mw-100 mx-auto">
+						<article className="card-body mx-auto py-0">
+							<form onSubmit={handleSubmit}>
+								<div className="form-group input-group mb-0">
+									<div className="input-group-prepend">
+										<span className="input-group-text">
+											<i className="fas fa-user" />
+										</span>
+									</div>
+									<input
+										type="text"
+										value={inputFname}
+										placeholder="Nombre"
+										onChange={e => setFnameValue(e.target.value)}
+										className="form-control"
+										required
+									/>
 								</div>
-								<input
-									type="text"
-									value={inputSname}
-									placeholder="Apellido"
-									onChange={e => setSnameValue(e.target.value)}
-									className="form-control"
-									required
-								/>
-							</div>
 
-							<div className="form-group input-group mt-3 mb-0">
-								<div className="input-group-prepend">
-									<span className="input-group-text">
-										<i className="far fa-envelope" />
-									</span>
+								<div className="form-group input-group mt-3 mb-0">
+									<div className="input-group-prepend">
+										<span className="input-group-text">
+											<i className="fas fa-user" />
+										</span>
+									</div>
+									<input
+										type="text"
+										value={inputSname}
+										placeholder="Apellido"
+										onChange={e => setSnameValue(e.target.value)}
+										className="form-control"
+										required
+									/>
 								</div>
-								<input
-									type="email"
-									id="Email"
-									value={inputEmail}
-									onChange={e => setEmailValue(e.target.value)}
-									placeholder="Correo"
-									className="form-control"
-									required
-								/>
-							</div>
 
-							<div className="form-group input-group mt-3 mb-0">
-								<div className="input-group-prepend">
-									<span className="input-group-text">
-										<i className="fas fa-lock" />
-									</span>
+								<div className="form-group input-group mt-3 mb-0">
+									<div className="input-group-prepend">
+										<span className="input-group-text">
+											<i className="far fa-envelope" />
+										</span>
+									</div>
+									<input
+										type="email"
+										id="Email"
+										value={inputEmail}
+										onChange={e => setEmailValue(e.target.value)}
+										placeholder="Correo"
+										className="form-control"
+										required
+									/>
 								</div>
-								<input
-									type="password"
-									id="Password"
-									value={inputPassword}
-									onChange={e => setPasswordValue(e.target.value)}
-									placeholder="Contraseña"
-									className="form-control"
-									required
-								/>
-							</div>
 
-							<div className="form-group text-center mt-3">
-								<button className="btn btn-outline-primary text-font-base btn-block">
-									Registrarse
-								</button>
-							</div>
-							<div className="form-group text-center text-font-base">
-								<p className="m-0">
-									¿Ya estás registrado?
-									<br />
-									<Link to="/Login">Ingresa aquí</Link>
-								</p>
-							</div>
-						</form>
-					</article>
+								<div className="form-group input-group mt-3 mb-0">
+									<div className="input-group-prepend">
+										<span className="input-group-text">
+											<i className="fas fa-lock" />
+										</span>
+									</div>
+									<input
+										type="password"
+										id="password"
+										value={inputPassword}
+										onChange={e => setPasswordValue(e.target.value)}
+										placeholder="Contraseña"
+										className="form-control"
+										required
+									/>
+								</div>
+
+								<div className="form-group text-center mt-3">
+									<button id="Rbutton" className="btn btn-outline-primary text-font-base btn-block">
+										Registrarse
+									</button>
+								</div>
+								<div className="form-group text-center text-font-base">
+									<p className="m-0 text-white">
+										¿Ya estás registrado?
+										<br />
+										<Link to="/Login">Ingresa aquí</Link>
+									</p>
+								</div>
+							</form>
+						</article>
+					</div>
 				</div>
 			</section>
 		);
