81e96a5cd5fc5e462d7c9fdb14f86dd93cb13d25
Mivargasg <miguelvargasgonzalezcr@gmail.com>
2021-04-29 01:08:22 +0000

    K

--- src/front/js/pages/forgotpassword.js
+++ src/front/js/pages/forgotpassword.js
@@ -2,24 +2,33 @@ import React from "react";
 
 export const ForgotPassword = () => {
 	return (
-		<div className="container">
-			<h2>INGRESE SU CORREO REGISTRADO</h2>
-			<article className="card-body mx-auto py-0">
-				<div className="jumbotron">
-					<div className="mb-5 row">
-						<div className="col-sm-12">
-							<input type="password" className="form-control" id="inputPassword" />
-						</div>
-					</div>
-					<div className="mb-5 row">
-						<div className="col-sm-12">
-							<button type="button" className="btn btn-secondary">
-								GENERAR CONTRASEÑA
-							</button>
-						</div>
-					</div>
+		<section id="R1" className="container">
+			<div className="text-center mt-3">
+				<h2 className="mb-0 mt-3 text-font-base text-white">RECUPERAR CONTRASEÑA</h2>
+				<br />
+			</div>
+			<div id="R2" className="card-contact card border-5 border-white mw-100 mx-auto">
+				<div id="R3" className="card-contact card mw-100 mx-auto">
+					<article className="card-body mx-auto py-0">
+						<form>
+							<div className="form-group input-group mt-3 mb-0">
+								<div className="input-group-prepend">
+									<span className="input-group-text">
+										<i className="far fa-envelope" />
+									</span>
+								</div>
+								<input type="email" id="Email" placeholder="Correo" className="form-control" required />
+							</div>
+
+							<div className="form-group text-center mt-3">
+								<button id="Rbutton" className="btn btn-outline-primary text-font-base btn-block">
+									GENERAR CONTRASEÑA
+								</button>
+							</div>
+						</form>
+					</article>
 				</div>
-			</article>
-		</div>
+			</div>
+		</section>
 	);
 };
