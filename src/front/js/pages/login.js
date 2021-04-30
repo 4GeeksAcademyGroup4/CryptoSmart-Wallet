bc3b5cf1eac474b43270d56ffd59e6217cabe3d5
hirolabpro <hirocastro@gmail.com>
2021-04-30 18:08:16 +0000

    Login Style

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -52,63 +52,62 @@ export const Login = () => {
 		return <Redirect to={{ pathname: "/Home" }} />;
 	} else {
 		return (
-			<section className="container mw-90">
-				<div className="card-contact card border-0 mw-100 mx-auto bg-light">
-					<div className="text-center mt-3">
-						<h2 className="mb-0 mt-3 text-font-base">Acceso</h2>
-						<p className="text-font-base">Bienvenido...</p>
+			<section id="R1" className="container ">
+				<div className="text-center mt-3">
+					<h2 className="mb-0 mt-3 text-font-base text-white">Ingresa a CryptoSmart Wallet</h2>
+					<br />
+				</div>
+				<div id="R2" className="card-contact card border-5 border-white mw-100 mx-auto">
+					<div id="R3login" className="card-contact card mw-100 mx-auto">
+						<article className="card-body mx-auto py-0">
+							<form onSubmit={handleSubmit}>
+								<div className="form-group input-group mb-0">
+									<div className="input-group-prepend">
+										<span className="input-group-text">
+											<i className="fas fa-user" />
+										</span>
+									</div>
+									<input
+										type="email"
+										id="Email"
+										value={inputEmail}
+										onChange={e => setEmailValue(e.target.value)}
+										placeholder="Correo"
+										className="form-control"
+										required
+									/>
+								</div>
+
+								<div className="form-group input-group mt-3 mb-0">
+									<div className="input-group-prepend">
+										<span className="input-group-text">
+											<i className="fas fa-user" />
+										</span>
+									</div>
+									<input
+										type="password"
+										id="password"
+										value={inputPassword}
+										onChange={e => setPasswordValue(e.target.value)}
+										placeholder="Contraseña"
+										className="form-control"
+										required
+									/>
+								</div>
+
+								<div className="form-group text-center mt-3">
+									<button id="Rbutton" className="btn btn-outline-primary text-font-base btn-block">
+										Iniciar sesión
+									</button>
+								</div>
+								<div className="form-group text-center text-font-base">
+									<p className="m-0 text-white">
+										<Link to="/ForgotPassword">¿Olvidaste tu contraseña?</Link>
+									</p>
+								</div>
+							</form>
+						</article>
 					</div>
-					<article className="card-body mx-auto py-0">
-						<form className="" onSubmit={handleSubmit}>
-							<div className="form-group input-group mb-0">
-								<div className="input-group-prepend">
-									<span className="input-group-text">
-										<i className="far fa-user" />
-									</span>
-								</div>
-								<input
-									type="text"
-									id="Email"
-									value={inputEmail}
-									placeholder="Email"
-									onChange={e => setEmailValue(e.target.value)}
-									className="form-control"
-									required
-								/>
-							</div>
-							<div className="form-group input-group mt-3 mb-0">
-								<div className="input-group-prepend">
-									<span className="input-group-text">
-										<i className="fas fa-lock" />
-									</span>
-								</div>
-								<input
-									type="password"
-									id="Password"
-									value={inputPassword}
-									onChange={e => setPwdValue(e.target.value)}
-									placeholder="Contraseña"
-									className="form-control"
-									required
-								/>
-							</div>
-							<div className="form-group text-center mt-3">
-								<button className="btn btn-outline-primary text-font-base btn-block">Ingresar</button>
-							</div>
-							<div className="form-group text-center">
-								<p className="m-0">
-									<Link to="/Register" className="text-center text-font-base">
-										Registrarse como nuevo usuario
-									</Link>
-								</p>
-								<p className="m-0">
-									<Link to="/ForgotPassword" className="text-center text-font-base">
-										Olvido su contraseña?
-									</Link>
-								</p>
-							</div>
-						</form>
-					</article>
 				</div>
 			</section>
 		);
