33aadbb7b6e0775ec8a82ad6cf667644f7e2eee4
hirolabpro <hirocastro@gmail.com>
2021-05-06 15:23:36 +0000

    Vista History

--- src/front/js/services/cryptoaccount.js
+++ src/front/js/services/cryptoaccount.js
@@ -135,6 +135,30 @@ class CryptoAccounts {
 			})
 			.catch(error => console.log("error:", error));
 	}
+
+	async History(id) {
+		let baseURL = process.env.BACKEND_URL + "/api/History/" + id;
+		var myHeaders = new Headers();
+		var AuthUser = JSON.parse(localStorage.getItem("user"));
+		let AuthHeader = "Bearer " + AuthUser.token;
+
+		myHeaders.append("Authorization", AuthHeader);
+
+		var requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			redirect: "follow"
+		};
+
+		return await fetch(baseURL, requestOptions)
+			.then(res => {
+				return res.json();
+			})
+			.then(result => {
+				return result;
+			})
+			.catch(error => console.log("error:", error));
+	}
 }
 
 export default CryptoAccounts;
