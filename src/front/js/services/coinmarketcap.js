3549008b4f0f5f82e5b7ec8ce6727e4884bea4ed
johmstone <johmstone@gmail.com>
2021-05-09 22:52:26 +0000

    update CORS policy

--- src/front/js/services/coinmarketcap.js
+++ src/front/js/services/coinmarketcap.js
@@ -1,79 +1,85 @@
 class CoinMarketCap {
-	constructor() {}
+    constructor() { }
 
-	async Latest() {
-		var myHeaders = new Headers();
+    async Latest() {
+        var myHeaders = new Headers();
         myHeaders.append("X-CMC_PRO_API_KEY", "d23ea42b-4f22-47b3-8e28-4650f23c4096");
-        myHeaders.append("Access-Control-Allow-Origin","*");
+        myHeaders.append("Access-Control-Allow-Headers", "Content-Type");
+        myHeaders.append("Access-Control-Allow-Origin", "*");
+        myHeaders.append("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
 
-		var requestOptions = {
-			method: "GET",
-			headers: myHeaders,
-			redirect: "follow"
-		};
+        var requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
 
-		return await fetch(
-			"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=6",
-			requestOptions
-		)
-			.then(response => {
-				if (response.status === 200) {
-					return response.json();
-				}
-			})
-			.then(result => {
-				return result.data;
-			})
-			.catch(error => console.log("error", error));
-	}
+        return await fetch(
+            "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=6",
+            requestOptions
+        )
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+            })
+            .then(result => {
+                return result.data;
+            })
+            .catch(error => console.log("error", error));
+    }
 
-	async Detail(id) {
-		var myHeaders = new Headers();
+    async Detail(id) {
+        var myHeaders = new Headers();
         myHeaders.append("X-CMC_PRO_API_KEY", "d23ea42b-4f22-47b3-8e28-4650f23c4096");
-        myHeaders.append("Access-Control-Allow-Origin","*");
+        myHeaders.append("Access-Control-Allow-Headers", "Content-Type");
+        myHeaders.append("Access-Control-Allow-Origin", "*");
+        myHeaders.append("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
 
-		var requestOptions = {
-			method: "GET",
-			headers: myHeaders,
-			redirect: "follow"
-		};
+        var requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
 
-		return await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=" + id, requestOptions)
-			.then(response => {
-				if (response.status === 200) {
-					return response.json();
-				}
-			})
-			.then(result => {
-				return result.data[id.toString()];
-			})
-			.catch(error => console.log("error", error));
-	}
+        return await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=" + id, requestOptions)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+            })
+            .then(result => {
+                return result.data[id.toString()];
+            })
+            .catch(error => console.log("error", error));
+    }
 
-	async DetailQuote(id) {
-		var myHeaders = new Headers();
+    async DetailQuote(id) {
+        var myHeaders = new Headers();
         myHeaders.append("X-CMC_PRO_API_KEY", "d23ea42b-4f22-47b3-8e28-4650f23c4096");
-        myHeaders.append("Access-Control-Allow-Origin","*");
+        myHeaders.append("Access-Control-Allow-Headers", "Content-Type");
+        myHeaders.append("Access-Control-Allow-Origin", "*");
+        myHeaders.append("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
 
-		var requestOptions = {
-			method: "GET",
-			headers: myHeaders,
-			redirect: "follow"
-		};
+        var requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
 
-		return await fetch(
-			"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=" + id,
-			requestOptions
-		)
-			.then(response => {
-				if (response.status === 200) {
-					return response.json();
-				}
-			})
-			.then(result => {
-				return result.data[id.toString()].quote.USD;
-			})
-			.catch(error => console.log("error", error));
-	}
+        return await fetch(
+            "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=" + id,
+            requestOptions
+        )
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+            })
+            .then(result => {
+                return result.data[id.toString()].quote.USD;
+            })
+            .catch(error => console.log("error", error));
+    }
 }
 export default CoinMarketCap;
