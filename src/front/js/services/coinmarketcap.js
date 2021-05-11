f5a3600e002ad413755f2cef0ecb815c4f60a221
johmstone <61640703+johmstone@users.noreply.github.com>
2021-05-10 18:48:28 -0600

    Update coinmarketcap.js

--- src/front/js/services/coinmarketcap.js
+++ src/front/js/services/coinmarketcap.js
@@ -3,7 +3,7 @@ class CoinMarketCap {
 
 	async Latest() {
 		var myHeaders = new Headers();
-		myHeaders.append("X-CMC_PRO_API_KEY", "d23ea42b-4f22-47b3-8e28-4650f23c4096");
+		myHeaders.append("X-CMC_PRO_API_KEY", "0a3549c4-3d25-4c8c-8315-8ec2baa5b2b6");
 
 		var requestOptions = {
 			method: "GET",
@@ -28,7 +28,7 @@ class CoinMarketCap {
 
 	async Detail(id) {
 		var myHeaders = new Headers();
-		myHeaders.append("X-CMC_PRO_API_KEY", "d23ea42b-4f22-47b3-8e28-4650f23c4096");
+		myHeaders.append("X-CMC_PRO_API_KEY", "0a3549c4-3d25-4c8c-8315-8ec2baa5b2b6");
 
 		var requestOptions = {
 			method: "GET",
@@ -51,7 +51,7 @@ class CoinMarketCap {
 
 	async DetailQuote(id) {
 		var myHeaders = new Headers();
-		myHeaders.append("X-CMC_PRO_API_KEY", "d23ea42b-4f22-47b3-8e28-4650f23c4096");
+		myHeaders.append("X-CMC_PRO_API_KEY", "0a3549c4-3d25-4c8c-8315-8ec2baa5b2b6");
 
 		var requestOptions = {
 			method: "GET",
